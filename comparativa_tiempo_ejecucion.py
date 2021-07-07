e194eb65f7c5cdff105d6432cf1c1294804453fd
Ruben Tobar <rntobar41@gmail.com>
2021-07-07 08:42:14 +0200

    Añade grafico de tiempos de ejecución

--- comparativa_tiempo_ejecucion.py
+++ comparativa_tiempo_ejecucion.py
@@ -5,30 +5,33 @@ df = pd.read_csv('tiempos_ejecucion/tiempos_ejecucion.csv', sep=',')
 
 PLOTS_DIR = 'plots/'
 
-a = df.loc[df['algorithm'] == 'DQN']['time_min']
-b = df.loc[df['algorithm'] == 'A2C']['time_min']
-c = df.loc[df['algorithm'] == 'PPO']['time_min']
+# Timepos de ejecución en minutos
+a = df.loc[df['algorithm'] == 'DQN']['time_min']//60
+b = df.loc[df['algorithm'] == 'A2C']['time_min']//60
+c = df.loc[df['algorithm'] == 'PPO']['time_min']//60
 
-labels = ['SeaquestNoFrameskip-v4', 'BreakoutNoFrameskip-v4',
-          'QbertNoFrameskip-v4', 'SpaceInvadersNoFrameskip-v4']
- 
+labels = ['Seaquest', 'Breakout',
+          'Qbert', 'SpaceInvaders']
+
 x = np.arange(len(labels))  # the label locations
 width = 0.20  # the width of the bars
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(x + width * 0, a, width, label='DQN')
-rects2 = ax.bar(x + width * 1, b, width, label='A2C')
-rects3 = ax.bar(x + width * 2, c, width, label='PPO')
+fig, ax = plt.subplots(figsize=(9.7082, 6))
+rects1 = ax.bar(x + width * -1, a, width, label='DQN', color='#0e7a06')
+rects2 = ax.bar(x + width * 0, b, width, label='A2C', color='#2329cf')
+rects3 = ax.bar(x + width * 1, c, width, label='PPO', color='#c91016')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
+ax.set_ylabel('Tiempo ejecución en minutos')
+ax.set_xlabel('Entorno de ejecución')
+plt.suptitle('Tiempos de ejecución de los algoritmos para cada entorno')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
+plt.ylim([0, 120])
 
-ax.bar_label(rects1, padding=3)
-ax.bar_label(rects2, padding=3)
-ax.bar_label(rects3, padding=3)
+ax.bar_label(rects1, padding=7)
+ax.bar_label(rects2, padding=7)
+ax.bar_label(rects3, padding=7)
 
-plt.savefig(PLOTS_DIR + 'tiempos_ejecucuion.png', transparent=True, dpi=100)
+plt.savefig(PLOTS_DIR + 'tiempos_ejecucion.png', transparent=True, dpi=100)
